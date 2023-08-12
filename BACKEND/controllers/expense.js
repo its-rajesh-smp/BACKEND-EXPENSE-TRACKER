9f9f34a62d3d9a9f51dab80f1ca3917826c98948
its-rajesh-smp <its.rajeshsmp@gmail.com>
2023-08-12 16:39:45 +0530

    Roolback Expense

--- BACKEND/controllers/expense.js
+++ BACKEND/controllers/expense.js
@@ -1,20 +1,27 @@
-const { where } = require("sequelize");
 const Expense = require("../models/expense");
 const User = require("../models/user");
+const sequelize = require("../utils/db");
 
 exports.create = async (req, res) => {
+  const t = await sequelize.transaction();
   try {
     const { name, price, category, userEmail, updatedTotalExpense } = req.body;
 
-    const dbRes = await Expense.create({ name, price, category, userEmail });
+    const dbRes = await Expense.create(
+      { name, price, category, userEmail },
+      { transaction: t }
+    );
 
     await User.update(
       { totalExpense: updatedTotalExpense },
-      { where: { email: userEmail } }
+      { where: { email: userEmail }, transaction: t }
     );
+
+    await t.commit();
 
     res.send(dbRes);
   } catch (error) {
+    await t.rollback();
     console.log(error);
     res.status(404).send(error.message);
   }
@@ -23,17 +30,20 @@ exports.create = async (req, res) => {
 exports.get = async (req, res) => {};
 
 exports.delete = async (req, res) => {
+  const t = await sequelize.transaction();
   try {
     const { id, updatedTotalExpense, userEmail } = req.body;
 
-    await Expense.destroy({ where: { id: id } });
+    await Expense.destroy({ where: { id: id }, transaction: t });
     await User.update(
       { totalExpense: updatedTotalExpense },
-      { where: { email: userEmail } }
+      { where: { email: userEmail }, transaction: t }
     );
 
+    await t.commit();
     res.send({ id });
   } catch (error) {
+    await t.rollback();
     console.log(error);
     res.status(404).send(error.message);
   }
